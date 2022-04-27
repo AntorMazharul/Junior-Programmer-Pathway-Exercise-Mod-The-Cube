9639cd3a3896c87f4ffc5463ffc139ccec9d4e34
Md. Mazharul Islam Antor <antormazharul@gmail.com>
2022-04-27 08:40:20 +0600

    Exercise

---
+++ Mod The Cube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public Vector3 position;
+    public float localScaleFold;
+    public Color color;
+    public float rotationSpeedX;
+    
+    void Start()
+    {
+        transform.position = position;
+        transform.localScale = Vector3.one * localScaleFold;       
+        Material material = Renderer.material;
+        material.color = new Color(color.r, color.g, color.b, color.a);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(rotationSpeedX * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
